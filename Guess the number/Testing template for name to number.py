42e5aa799c7523e3167e8d90441e326e32e27a3a
transformer99 <narens99ap@gmail.com>
2015-04-24 23:33:06 +0530

    Added testing templates
    mentioned in code clinic tips

---
+++ Guess the number/Testing template for name to number.py
@@ -0,0 +1,23 @@
+# Testing template for name_to_number()
+
+###################################################
+# Copy and paste your definition of name_to_number() here
+
+
+###################################################
+# Test calls to name_to_number()
+print name_to_number("rock")
+print name_to_number("Spock")
+print name_to_number("paper")
+print name_to_number("lizard")
+print name_to_number("scissors")
+
+
+
+###################################################
+# Output to the console should have the form:
+# 0
+# 1
+# 2
+# 3
+# 4
---
+++ Guess the number/Testing template for number to name.py
@@ -0,0 +1,24 @@
+# Testing template for number_to_name()
+
+###################################################
+# Copy and paste your definition of number_to_name() here
+
+
+
+###################################################
+# Test calls to number_to_name()
+print number_to_name(0)
+print number_to_name(1)
+print number_to_name(2)
+print number_to_name(3)
+print number_to_name(4)
+
+
+
+###################################################
+# Output to the console should have the form:
+# rock
+# Spock
+# paper
+# lizard
+# scissors
